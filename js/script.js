e9869b88eb2f426a4d112dc9562e460e25df9e7d
Liline84 <duclesfredeline@gmail.com>
2025-12-21 21:17:45 -0500

    Dynamically load header and footer on DOMContentLoaded
    Add event listener to load header and footer dynamically

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const body = document.body;
+    body.classList.add('preload');
 
+    const headerContainer = document.getElementById("header");
+
+    // Injecter le header
+    fetch("header.html")
+        .then(res => res.text())
+        .then(data => {
+            headerContainer.innerHTML = data;
+
+            // Lancer l'app une fois le header chargé
+            if (typeof runMainAppLogic === 'function') {
+                runMainAppLogic();
+            }
+        })
+        .catch(err => console.error("Erreur d'injection du header:", err));
+
+    // Injecter le footer
+    fetch("footer.html")
+        .then(res => res.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch(err => console.error("Erreur d'injection du footer:", err));
+});
