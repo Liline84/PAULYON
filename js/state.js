f34b749e3558f4cbe95e8c617394d693c478843f
Liline84 <duclesfredeline@gmail.com>
2026-01-15 19:13:08 -0500

    Update state.js

--- js/state.js
+++ js/state.js
@@ -1,1 +1,13 @@
+// state.js
+export const state = {
+    translations: {},
+    dataConfig: {},
+    cart: [],
+    currentLanguage: localStorage.getItem('lang') || "fr"
+};
 
+// Fonction pour mettre à jour la langue
+export function updateLanguageState(lang) {
+    state.currentLanguage = lang;
+    localStorage.setItem('lang', lang);
+}
