e446ea3e320c03bf8fe7bf67ce744152183178aa
Liline84 <duclesfredeline@gmail.com>
2026-02-18 18:28:42 -0500

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,34 +1,17 @@
-export function generateServiceMenu(DOMElements, dataConfig) 
-    if (!DOMElements.serviceMenuContent || !dataConfig.services) return;
+// produits.js
+import { state } from './state.js';
 
-    DOMElements.serviceMenuContent.innerHTML = dataConfig.services.map(service => `
-        <li><a href="#service-{service.id}" class="nav-link">${service.title}</a></li>
-    `).join('');
-}
+export function renderProductCards() {
+    const container = document.querySelector('#products-section .cards-container');
+    if (!container || !state.dataConfig.products) return;
 
-export function renderServiceCards(DOMElements, dataConfig, translations, currentLanguage) {
-    const container = DOMElements.serviceCardsContainer;
-    if (!container || !dataConfig.services) return;
-    const currency = dataConfig.generalSettings.currentCurrency || ”;
-    const currentTrans = translations[currentLanguage] || translations["fr"];
-
-    container.innerHTML = dataConfig.services.map(service => `
-        <div class="card service-card{service.status === 'online' ? 'status-online' : 'status-offline'}" id="service-service.id">
-            <div class="card-icon{service.colorClass}">
-                <i class="service.iconClass"></i>
-            </div>
-            
-            <div class="card-body">
-                <h3>{service.title}</h3>
-                <p class="card-description">service.details</p>
-                
-                <div class="card-footer">
-                    <span class="card-price">{service.price} currency</span>
-                    <span class="card-status status-dot" data-i18n="status-{service.status}">
-                        currentTrans[`status-{service.status}`] || (service.status === 'online' ? 'En Ligne' : 'Hors Ligne')}
-                    </span>
-                </div>
-            </div>
+    container.innerHTML = state.dataConfig.products.map(product => `
+        <div class="card product-card">
+            <img src="${product.imagePath}" alt="${product.name}">
+            <h3>${product.name}</h3>
+            <button class="btn-add-cart" data-name="${product.name}" data-price="${product.price}">
+                Commander
+            </button>
         </div>
     `).join('');
 }
