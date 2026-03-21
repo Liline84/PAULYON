02cf14b625173cd0b97454f5a366310b36c1fb04
Liline84 <duclesfredeline@gmail.com>
2026-03-21 16:00:21 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
-        const headerHTML = await fetch("header.html").then(res => res.text());
+        const headerHTML = await fetch("/header.html").then(res => res.text());
         document.getElementById("header-container").innerHTML = headerHTML;
 
-        const footerHTML = await fetch("footer.html").then(res => res.text());
+        const footerHTML = await fetch("/footer.html").then(res => res.text());
         document.getElementById("footer-container").innerHTML = footerHTML;
 
         const [translations, config] = await Promise.all([
-            fetch("traduction.json").then(res => res.json()),
-            fetch("data.json").then(res => res.json())
+            fetch("/traduction.json").then(res => res.json()),
+            fetch("/data.json").then(res => res.json())
         ]);
 
         state.translations = translations;
